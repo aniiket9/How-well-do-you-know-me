00dddd2bbae125ff23e41ac88c0b31fb1f5e5c22
aniiket9 <86234717+aniiket9@users.noreply.github.com>
2022-11-15 17:03:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var readlineSync = require('readline-sync');
+
+var score = 0;
+
+var questions = [{
+  question: 'where do i work?',
+  answer: 'tekion'
+},{
+  question: 'where do i live?',
+  answer: 'bangalore'
+},{
+  question: 'Am i a vegeterian?',
+  answer: 'yes'
+}]
+
+function welcome(){
+  var userName = readlineSync.question('what is your name?')
+  console.log('Hi! ' + userName + ' Do you know Me?')  
+}
+
+function play(question, answer){
+  var userAnswer = readlineSync.question(question);
+  if(userAnswer.toUpperCase() === answer.toUpperCase()){
+    console.log('Right!')
+    score +=1;
+    console.log(score)
+  }
+  else{
+    console.log('Wrong!')
+    score -=1;
+  }
+}
+
+function game(){
+  for (var i=0; i<questions.length; i++) {
+    var currentquestion = questions[i];
+    play(currentquestion.question, currentquestion.answer)
+  }
+}
+
+
+
+welcome();
+game();
+if(score === 3){
+  console.log('You know well about me')
+}else{
+  console.log('you do not know much about me')
+
